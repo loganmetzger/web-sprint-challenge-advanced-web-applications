c16a86fe5f4ec20157f8b745a329cb6f3d66f1b1
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-08-21 11:23:19 -0400

    fleshed out form and handle functions, changed username and password in server to make my life easier

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+import { axiosWithAuth } from './axiosWithAuth'
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [form, setForm] = useState({
+    username: '',
+    password: ''
+  });
+
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axiosWithAuth().post('/api/login', form)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => console.log(err))
+  }
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input 
+          name='username'
+          type='text'
+          placeholder='Username'
+          onChange={handleChange}
+        />
+        <input 
+          name='password'
+          type='text'
+          placeholder='Password'
+          onChange={handleChange}
+        />
+        <button>Login</button>
+      </form>
+    </div>
   );
 };
 
